5cac4256015aa3f7ee64f8585379a7fa10f6d5af
SurajSSingh <surajss2@illinois.edu>
2022-10-03 14:35:36 -0700

    Create Ball, Paddle, and Game scripts
    Initial code for ball, paddle, and game manager

---
+++ Assets/Scripts/Provided/Ball.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Ball : MonoBehaviour
+{
+    // Ball components
+    private Rigidbody2D rb;
+
+    [Header("Ball Attributes")]
+    [SerializeField] float speed;
+    [SerializeField] Vector3 startPosition = Vector3.zero;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
+    }
+
+    private void OnEnable()
+    {
+        transform.position = startPosition;
+        rb.velocity = Vector2.zero;
+    }
+
+    /// <summary>
+    /// Launches the ball in a random direction.
+    /// </summary>
+    /// <param name="launchRight">
+    /// A bool which determines if the ball goes right (if true) or left (if false), defaults to true
+    /// </param>
+    public void LaunchBall(bool launchRight = true)
+    {
+        // launchRight ? 1 : -1 means if launchRight is true then use 1 else use -1
+        Vector2 direction = new Vector2(launchRight ? 1 : -1,
+                                        Random.Range(-100f, 100f)
+                                       ).normalized;
+        rb.velocity = direction * speed;
+    }
+}
---
+++ Assets/Scripts/Provided/Paddle.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Paddle : MonoBehaviour
+{
+    // Paddle components
+    private Rigidbody2D rb;
+
+    [Header("Paddle Attributes")]
+    [SerializeField] float speed;
+    [SerializeField] Vector3 startPosition = Vector3.zero;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (startPosition == Vector3.zero)
+        {
+            startPosition = transform.position;
+        }
+    }
+
+    private void OnEnable()
+    {
+        transform.position = startPosition;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void UpdateMovement(bool moveUp = true)
+    {
+        Vector2 direction = moveUp ? Vector2.up : Vector2.down;
+        rb.velocity = direction * speed;
+    }
+}
