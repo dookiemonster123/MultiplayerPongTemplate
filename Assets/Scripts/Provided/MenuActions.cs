dd0bdc0d1f22217592322f729bbfd55ae51b4f45
SurajSSingh <surajss2@illinois.edu>
2022-10-03 12:47:44 -0700

    Complete Simple Main Menu
    Main menu created with start buttons for each of multiplayer game scenes and a quit button

---
+++ Assets/Scripts/Provided/MenuActions.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+    public void ReloadCurrentLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void LoadLevel(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        // If you are running this script from the Unity Editor,
+        // stop the editor to simulate quitting
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
