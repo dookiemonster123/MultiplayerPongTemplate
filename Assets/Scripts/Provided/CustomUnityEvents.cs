87d1a7deff3ab54a449e9108ed73bef885d08904
SurajSSingh <surajss2@illinois.edu>
2022-10-03 21:49:33 -0700

    Update CustomUnityEvents.cs
    Serialize Custom Events

--- Assets/Scripts/Provided/CustomUnityEvents.cs
+++ Assets/Scripts/Provided/CustomUnityEvents.cs
@@ -1,3 +1,5 @@
 using UnityEngine.Events;
+[System.Serializable]
 public class UnityEvent_PlayerWin : UnityEvent<PlayerWin> { }
+[System.Serializable]
 public class UnityEvent_Int : UnityEvent<int> { }
